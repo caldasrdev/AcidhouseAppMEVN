8138e368bbb0a41ab53a5fb58b2664fed03f2202
Daniel Caldas <zthebancaldas@gmail.com>
2021-10-21 19:29:52 -0500

    main

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9d25ae36864631e03f9a26ffeeab4e40'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '13e0a88a-ebf5-45a3-828f-a6de71f30524'
+};
